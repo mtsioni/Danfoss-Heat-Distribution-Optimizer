96c50106243ca2b3d9b4418e07049be3ec689075
Daniel Falat <danfa007@gmail.com>
2026-03-22 13:23:53 +0100

    SCRUM-12 GUI

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
 
 namespace Danfoss_Heat_Distribution_Optimizer.Views;
 
@@ -7,5 +10,24 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        // this sets the default theme to light when the app launched
+        Application.Current!.RequestedThemeVariant = ThemeVariant.Light;
+    }
+
+    // this function is called when the theme toggle is clicked and it changes the theme
+    private void ThemeToggle_CheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        var toggle = sender as ToggleSwitch;
+        
+        if (toggle.IsChecked == true)
+        {
+            Application.Current.RequestedThemeVariant = ThemeVariant.Dark;
+        }
+        
+        else
+        {
+            Application.Current.RequestedThemeVariant = ThemeVariant.Light;
+        }
     }
 }
