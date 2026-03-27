0eb5774837b6ef9470454e569aeaf475906ab212
mtsioni <thalia.tsioni@gmail.com>
2026-03-27 03:52:44 +0100

    SCRUM-53 Create Grid class in Models folder

---
+++ Models/Grid.cs
@@ -0,0 +1,11 @@
+namespace Danfoss_Heat_Distribution_Optimizer.Models
+{
+    public class Grid
+    {
+        public int GridID { get; set; }
+        public string? Name { get; set; }
+        public int Population { get; set; }
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
