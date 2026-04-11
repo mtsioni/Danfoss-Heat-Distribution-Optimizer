7ff202366a97464c39be54ab0daaaca0a031ceb1
Pavel_qrm <telefon3333v0.2@gmail.com>
2026-04-11 20:21:52 +0200

    Updated Unit Models
    Added constructors to ElectricUnit and HybridUnit

--- Models/HybridUnit.cs
+++ Models/HybridUnit.cs
@@ -1,3 +1,5 @@
+using System.Reactive;
+using System.Reflection.PortableExecutable;
 using Danfoss_Heat_Distribution_Optimizer.Services.Interfaces;
 
 namespace Danfoss_Heat_Distribution_Optimizer.Models
@@ -30,5 +32,28 @@ namespace Danfoss_Heat_Distribution_Optimizer.Models
         public TimeSeries<double> ElectricityRecords { get; set; }
         public TimeSeries<double> PollutionRecords { get; set; }
 
+        public HybridUnit(int unitID, string name, double maxElectricity, string fuelName, 
+        double fuelConsumption, double emissions, double maxHeat, double productionCost, 
+        double netProductionCost, double netHeat, double netElectricity, double netPollution, 
+        TimeSeries<double> productionCostRecords, TimeSeries<double> heatRecords, 
+        TimeSeries<double> electricityRecords, TimeSeries<double> pollutionRecords)
+        {
+            UnitID = unitID;
+            Name = name;
+            MaxElectricity = maxElectricity;
+            FuelName = fuelName;
+            FuelConsumption = fuelConsumption;
+            Emissions = emissions;
+            MaxHeat = maxHeat;
+            ProductionCost = productionCost;
+            NetProductionCost = netProductionCost;
+            NetHeat = netHeat;
+            NetElectricity = netElectricity;
+            NetPollution = netPollution;
+            ProductionCostRecords = productionCostRecords;
+            HeatRecords = heatRecords;
+            ElectricityRecords = electricityRecords;
+            PollutionRecords = pollutionRecords;
+        }
     }
 }
