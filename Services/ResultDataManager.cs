2e1aeb672b1cf57866128f15c4aed291fd0ced46
mtsioni <thalia.tsioni@gmail.com>
2026-04-16 16:13:16 +0200

    SCRUM-90 Result Data Manager implementation
    - a method to be called by Optimizer
    - a method to write to CSV by calling the ResultDataSaver
    - a method called by DataVisualizer to give results into IOptimizedUnit format

--- Services/ResultDataManager.cs
+++ Services/ResultDataManager.cs
@@ -3,30 +3,67 @@ using Danfoss_Heat_Distribution_Optimizer.Models;
 using Danfoss_Heat_Distribution_Optimizer.Data;
 using System;
 using Danfoss_Heat_Distribution_Optimizer.Services.Interfaces;
-using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
-using CsvHelper.Configuration.Attributes;
 
 namespace Danfoss_Heat_Distribution_Optimizer.Services
 {
     public static class ResultDataManager
     {
-        public static DateTime OptimizationPeriodStart {get;set;}
-        public static DateTime OptimizationPeriodEnd {get;set;}
-        public static int TimeResolution {get;set;}
-        private static List<IOptimizedUnit>? _resultData {get;set;}
-        private static ResultDataSaver? _dataSaver {get;set;}
+        public static DateTime OptimizationPeriodStart { get; set; }
+        public static DateTime OptimizationPeriodEnd { get; set; }
+        public static int TimeResolution { get; set; }
+        private static List<IOptimizedUnit>? _resultData { get; set; }
+        private static ResultDataSaver? _dataSaver { get; set; }
+
+        // called by Optimizer
+        public static void ProcessResults(List<IOptimizedUnit> results)
+        {
+            _resultData = results;
+            SaveResultData();
+        }
 
         private static void SaveResultData()
         {
-            
+            if (_resultData == null || _resultData.Count == 0)
+                return;
+
+            foreach (var unit in _resultData)
+            {
+                List<ResultDataDTO> hourlyRecords = new List<ResultDataDTO>();
+
+                // Calculate total hours from optimization period
+                int totalHours = (int)(OptimizationPeriodEnd - OptimizationPeriodStart).TotalHours;
+
+                // Extract hourly data from TimeSeries properties
+                for (int i = 0; i < totalHours; i++)
+                {
+                    DateTime currentTime = OptimizationPeriodStart.AddMinutes(i * TimeResolution);
+
+                    var dto = new ResultDataDTO
+                    {
+                        UnitName = unit.Name,
+                        Time = currentTime,
+                        HeatMWh = unit.Heat[currentTime],
+                        Electricity = unit.Electricity[currentTime],
+                        ProductionCost = unit.ProductionCost[currentTime],
+                        // PrimaryEnergy = unit.PrimaryEnergy[currentTime],
+                        // Co2Emissions = unit.Co2Emissions[currentTime],
+                        NetCost = unit.NetProductionCost  // Total net cost for this unit
+                    };
+
+                    hourlyRecords.Add(dto);
+                }
+
+                ResultDataSaver.SaveToCSV(unit.Name, hourlyRecords);
+            }
         }
 
-        public static TimeSeries<IOptimizedUnit> GetResultData()
+        // called by DataVisualizer
+        public static List<IOptimizedUnit> GetResultData()
         {
-            TimeSeries<IOptimizedUnit> ResultData = new TimeSeries<IOptimizedUnit>();
+            if (_resultData == null)
+                return new List<IOptimizedUnit>();
 
-            return ResultData;
+            return _resultData;
         }
     }
 }
